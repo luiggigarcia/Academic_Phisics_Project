289bdfaf6ecc0d7a47fd3d73b19ae67f5c11fd2c
Luiggi Paschoalini Garcia <83616830+luiggigarcia@users.noreply.github.com>
2022-11-10 17:52:58 -0300

    Delete graphics.py

--- graphics.py
+++
@@ -1,8 +0,0 @@
-from information import *
-
-initialData = beforeIntercept()
-ball, robot = initialData[0], initialData[1]  
-
-interceptData = afterIntercept()
-data_interception, ballIntercept, robotIntercept = interceptData[0], interceptData[1], interceptData[2]
-
